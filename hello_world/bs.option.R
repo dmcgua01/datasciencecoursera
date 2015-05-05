d423edd6be049f8a1b063e4067062eb9c7bb7be8
Dan McGuane <daniel.mcguane@gmail.com>
2015-05-05 00:08:26 -0400

    initial commit

---
+++ hello_world/bs.option.R
@@ -0,0 +1,9 @@
+#taken from the slides on Intro to R Programming
+
+# price of call option
+callprice.bs <- function (s, x, r, sigma, t.exp, t) {
+  d.pos <- log(s/x) + (r + 0.5 * sigma^2) * (t.exp - t)
+  d.pos <- d.pos/(sigma * (t.exp - t)^0.5)
+  d.neg <- d.pos - sigma * (t.exp - t)^0.5
+  s * pnorm(d.pos) - x * exp( - r * (t.exp - t)) * pnorm(d.neg)
+}
